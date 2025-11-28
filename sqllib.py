68591bf4ccb6e623e0d6f02d5191c04b909c2d30
Pat Jones <patjones80@users.noreply.github.com>
2025-11-27 23:27:52 -0800

    Update sqllib.py

--- sqllib.py
+++ sqllib.py
@@ -6,107 +6,102 @@ import psycopg2
 import sys
 
 def eprint(*args, **kwargs):
-    ''' print to apache error log; use for exception debugging
+    ''' Print to Apache error log; use for exception debugging
     '''
     print(*args, file=sys.stderr, **kwargs)
 
 def db_connect_pg():
-        ''' make connection to postgres
-        '''
-
-        try:
-                return psycopg2.connect(dbname="covid") #, user="django_read_write", password="8gPA>+5AsYaH7uh]")
-        except Exception as e:
-                print('Problem with db connection:', e)
+    ''' Make connection to postgres
+    '''
+    try:
+        return psycopg2.connect(dbname="covid")
+    except Exception as e:
+        print('Problem with db connection:', e)
 
 def get_national():
-        ''' query state-level data table
-                aggregate up to national level
+    ''' Query state-level data table and aggregate up to national level
+    
+        Return: list of tuples that contain - 
+                0: updated on
+                1: total cases
+                2: total deaths
+                3: current cases
+                4: current deaths
+                5: rolling average (cases)
+    '''
+    conn = db_connect_pg()
+    c = conn.cursor()
 
-                return: list of tuples that contain - 
-                                0: updated on
-                                1: total cases
-                                2: total deaths
-                                3: current cases
-                                4: current deaths
-                                5: rolling average (cases)
-        '''
+    sql = '''--First build up a national-level cte
+             WITH cte_us AS
+             (
+                    SELECT updated_on, SUM(cases) AS cases, SUM(deaths) AS deaths
+                    FROM state_data
+                    GROUP BY updated_on
+             )
 
-        conn = db_connect_pg()
-        c = conn.cursor()
+             --Now we can query off that like any state-level query
+             SELECT t_1.*
+                  , ROUND(AVG(t_1.new_cases) OVER (ORDER BY t_1.updated_on ROWS BETWEEN 7 PRECEDING AND CURRENT ROW))::integer AS rolling_avg
+             FROM (SELECT a.updated_on, 
+                          a.cases, 
+                          a.deaths,
+                          a.cases  - b.cases  AS new_cases,
+                          a.deaths - b.deaths AS new_deaths
+                   FROM cte_us a
+                        INNER JOIN cte_us b ON (a.updated_on::date - b.updated_on::date) = 1) t_1
+             WHERE t_1.new_cases > 0                        
+             ORDER BY t_1.updated_on;''' 
 
-        sql = '''--First build up a national-level cte
-                         WITH cte_us AS
-                         (
-                                SELECT updated_on, SUM(cases) AS cases, SUM(deaths) AS deaths
-                                FROM state_data
-                                GROUP BY updated_on
-                         )
-
-                         --Now we can query off that like any state-level query
-                         SELECT t_1.*,
-                                     ROUND(AVG(t_1.new_cases) OVER (ORDER BY t_1.updated_on ROWS BETWEEN 7 PRECEDING AND CURRENT ROW))::integer AS rolling_avg
-                         FROM (SELECT a.updated_on, 
-                                                  a.cases, 
-                                                  a.deaths,
-                                                  a.cases  - b.cases  AS new_cases,
-                                                  a.deaths - b.deaths AS new_deaths
-                                    FROM cte_us a
-                                                INNER JOIN cte_us b ON (a.updated_on::date - b.updated_on::date) = 1) t_1
-                         WHERE t_1.new_cases > 0                        
-                     ORDER BY t_1.updated_on;''' 
-
-        c.execute(sql)
-        result = c.fetchall()
-        conn.close()
-
-        return result
+    c.execute(sql)
+    result = c.fetchall()
+    conn.close()
+    
+    return result
         
 def get_state_rollup(us_state):
-        ''' query state-level data table
-                takes: verbose state name
+    ''' Query state-level data table
+        Takes: verbose state name
+        Return: list of tuples that contain - 
+                0: updated on
+                1: county (blank for state level table)
+                2: state (verbose)
+                3: fips_code
+                4: total cases
+                5: total deaths
+                6: current cases
+                7: current deaths
+                8: rolling average
+    '''
+    conn = db_connect_pg()
+    c = conn.cursor()
 
-                return: list of tuples that contain - 
-                                0: updated on
-                                1: county (blank for state level table)
-                                2: state (verbose)
-                                3: fips_code
-                                4: total cases
-                                5: total deaths
-                                6: current cases
-                                7: current deaths
-                                8: rolling average
-        '''
-
-        conn = db_connect_pg()
-        c = conn.cursor()
-
-        sql = '''SELECT c.*, ROUND(AVG(c.new_cases) OVER (ORDER BY c.updated_on ROWS BETWEEN 7 PRECEDING AND CURRENT ROW)) AS rolling_avg
-                         FROM (SELECT a.*, 
-                                                  a.cases  - b.cases  AS new_cases, 
-                                                  a.deaths - b.deaths AS new_deaths
-                                   FROM state_data a INNER JOIN state_data b ON julianday(a.updated_on) - julianday(b.updated_on) = 1.0 AND a.state = b.state
-                                   WHERE a.state = '{}') c
-                         ORDER BY c.updated_on;'''.format(us_state)
+    sql = '''SELECT c.*, ROUND(AVG(c.new_cases) OVER (ORDER BY c.updated_on ROWS BETWEEN 7 PRECEDING AND CURRENT ROW)) AS rolling_avg
+             FROM (SELECT a.*, 
+                          a.cases  - b.cases  AS new_cases, 
+                          a.deaths - b.deaths AS new_deaths
+                   FROM state_data a INNER JOIN state_data b ON julianday(a.updated_on) - julianday(b.updated_on) = 1.0 AND a.state = b.state
+                   WHERE a.state = '{}') c
+             ORDER BY c.updated_on;'''.format(us_state)
 
 
-        sql_pg = '''SELECT t_1.*,
-                           ROUND(AVG(t_1.new_cases) OVER (ORDER BY t_1.updated_on ROWS BETWEEN 7 PRECEDING AND CURRENT ROW))::integer AS rolling_avg
+    sql_pg = '''SELECT t_1.*,
+                       ROUND(AVG(t_1.new_cases) OVER (ORDER BY t_1.updated_on ROWS BETWEEN 7 PRECEDING AND CURRENT ROW))::integer AS rolling_avg
                 FROM (SELECT a.updated_on, 
-                                                         a.cases, 
-                                                         a.deaths,
-                                         a.cases  - b.cases  AS new_cases,
+                             a.cases, 
+                             a.deaths,
+                             a.cases  - b.cases  AS new_cases,
                              a.deaths - b.deaths AS new_deaths
-                                    FROM state_data a
-                                      INNER JOIN state_data b ON (a.updated_on::date - b.updated_on::date) = 1 AND a.state = b.state AND a.state = '{}') t_1
-                            WHERE t_1.new_cases > 0
-                                ORDER BY t_1.updated_on;'''.format(us_state)
+                      FROM state_data a
+                           INNER JOIN state_data b ON (a.updated_on::date - b.updated_on::date) = 1 AND a.state = b.state AND a.state = '{}') t_1
+                WHERE t_1.new_cases > 0
+                ORDER BY t_1.updated_on;'''.format(us_state)
 
-        c.execute(sql_pg)
-        result = c.fetchall()
-        conn.close()
+    c.execute(sql_pg)
+    result = c.fetchall()
+    conn.close()
 
-        return result
+    return result
 
 def get_county_level(us_state):
         ''' get the county-level list for the selected state
